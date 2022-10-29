61fcfa8aa9c907414a03159be75b7329a2935fbd
András Kovács <k.andras9@gmail.com>
2022-10-29 18:13:09 +0200

    more policies implemented policies tested with http requests

--- WebApi/Controllers/TestController.cs
+++ WebApi/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers;
@@ -8,9 +9,35 @@ namespace WebApi.Controllers;
 [Authorize]
 public class TestController : ControllerBase
 {
+    [HttpGet("allowanonymous"), AllowAnonymous]
+    public ActionResult GetAsAnonymous()
+    {
+        return Ok("This was accepted as anonymous");
+    }
+    
     [HttpGet("authorized")]
     public ActionResult GetAsAuthorized()
     {
         return Ok("This was accepted as authorized");
+    }
+
+    [HttpGet("mustbevia"), Authorize("mustbevia")]
+    public ActionResult GetAsVia()
+    {
+        return Ok("This was accepted as via domain");
+    }
+
+    [HttpGet("manualcheck")]
+    public ActionResult GetWithManualCheck()
+    {
+        Claim? claim = User.Claims.FirstOrDefault(claim => claim.Type.Equals(ClaimTypes.Role));
+
+        if (claim == null)
+            return StatusCode(403, "You have no role");
+
+        if (!claim.Value.Equals("Teacher"))
+            return StatusCode(403, "You're not a teacher");
+
+        return Ok("You are a teacher, you may proceed");
     }
 }
