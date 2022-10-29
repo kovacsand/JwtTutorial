cc11aa31e7bddc57ebfb1b84a1ad4e9616edf958
András Kovács <k.andras9@gmail.com>
2022-10-29 17:13:21 +0200

    policy guarding

---
+++ WebApi/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Authorize]
+public class TestController : ControllerBase
+{
+    [HttpGet("authorized")]
+    public ActionResult GetAsAuthorized()
+    {
+        return Ok("This was accepted as authorized");
+    }
+}
