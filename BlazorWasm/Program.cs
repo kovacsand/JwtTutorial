b1671e1b9c15dacaa04924d64c5fe5e20c8063c6
András Kovács <k.andras9@gmail.com>
2022-10-30 22:27:38 +0100

    auth state provider implemented

--- BlazorWasm/Program.cs
+++ BlazorWasm/Program.cs
@@ -1,8 +1,12 @@
+using System.Net;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWasm;
+using BlazorWasm.Auth;
 using BlazorWasm.Services;
 using BlazorWasm.Services.Http;
+using Microsoft.AspNetCore.Components.Authorization;
+using Shared.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +15,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<IAuthService, JwtAuthService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
+
+AuthorizationPolicies.AddPolicies(builder.Services);
 
 await builder.Build().RunAsync();
