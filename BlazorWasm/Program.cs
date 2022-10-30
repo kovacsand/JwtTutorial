90b4f5c255db311e74408416f5a731431ba932b6
András Kovács <k.andras9@gmail.com>
2022-10-30 14:55:58 +0100

    authservice login implemented

--- BlazorWasm/Program.cs
+++ BlazorWasm/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWasm;
+using BlazorWasm.Services;
+using BlazorWasm.Services.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<IAuthService, JwtAuthService>();
 
 await builder.Build().RunAsync();
---
+++ BlazorWasm/Services/Http/JwtAuthService.cs
@@ -0,0 +1,94 @@
+﻿using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using Shared.Dtos;
+using Shared.Models;
+
+namespace BlazorWasm.Services.Http;
+
+public class JwtAuthService : IAuthService
+{
+    private readonly HttpClient client = new();
+
+    //For caching
+    public static string? Jwt { get; private set; } = "";
+    
+    public Action<ClaimsPrincipal> OnAuthStateChanged { get; set; } = null!;
+    
+    // Below methods "borrowed" from https://github.com/SteveSandersonMS/presentation-2019-06-NDCOslo/blob/master/demos/MissionControl/MissionControl.Client/Util/ServiceExtensions.cs
+    private static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+    {
+        string payload = jwt.Split('.')[1];
+        byte[] jsonBytes = ParseBase64WithoutPadding(payload);
+        Dictionary<string, object>? keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+        return keyValuePairs!.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()!));
+    }
+    
+    private static byte[] ParseBase64WithoutPadding(string base64)
+    {
+        switch (base64.Length % 4)
+        {
+            case 2:
+                base64 += "==";
+                break;
+            case 3:
+                base64 += "=";
+                break;
+        }
+
+        return Convert.FromBase64String(base64);
+    }
+    
+    private static ClaimsPrincipal CreateClaimsPrincipal()
+    {
+        if (string.IsNullOrEmpty(Jwt))
+            return new ClaimsPrincipal();
+
+        IEnumerable<Claim> claims = ParseClaimsFromJwt(Jwt);
+    
+        ClaimsIdentity identity = new(claims, "jwt");
+
+        ClaimsPrincipal principal = new(identity);
+        return principal;
+    }
+    
+    public async Task LoginAsync(string username, string password)
+    {
+        UserLoginDto userLoginDto = new()
+        {
+            Username = username,
+            Password = password
+        };
+
+        string userAsJson = JsonSerializer.Serialize(userLoginDto);
+        StringContent content = new(userAsJson, Encoding.UTF8, "application/json");
+
+        HttpResponseMessage response = await client.PostAsync("https://localhost:7100/auth/login", content);
+        string responseContent = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+            throw new Exception(responseContent);
+
+        string token = responseContent;
+        Jwt = token;
+
+        ClaimsPrincipal principal = CreateClaimsPrincipal();
+        
+        OnAuthStateChanged.Invoke(principal);
+    }
+
+    public Task LogoutAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RegisterAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ClaimsPrincipal> GetAuthAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
